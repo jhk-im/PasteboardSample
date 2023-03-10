8e821cc86a225ed8760ffc90f7b45071543f9f8a
jhk-im <kjhun200@gmail.com>
2023-03-10 20:45:31 +0900

    feat: preview 추가

--- PasteboardSample/ViewController.swift
+++ PasteboardSample/ViewController.swift
@@ -6,37 +6,70 @@
 //
 
 import UIKit
+import LinkPresentation
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var textlLabel: UILabel!
-    @IBOutlet weak var urlLabel: UILabel!
-    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var lbText: UILabel!
+    @IBOutlet weak var lbUrl: UILabel!
+    
+    
+    @IBOutlet weak var preview: UIView!
+    @IBOutlet weak var ivImage: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         let pasteboard = UIPasteboard.general
         
-        if pasteboard.image != nil {
+        if pasteboard.hasImages {
             guard let url = URL(string: pasteboard.string ?? "") else { return }
-            imageView.load(url: url) {
-                
+            ivImage.load(url: url) {
+
             }
-        } else if pasteboard.url != nil {
-            urlLabel.text = pasteboard.string ?? ""
+        } else if pasteboard.hasURLs {
+            lbUrl.text = pasteboard.string ?? ""
+            fetchPreview(urlString: pasteboard.string ?? "")
         } else {
-            textlLabel.text = pasteboard.string ?? ""
+            lbText.text = pasteboard.string ?? ""
         }
-        
         //print("numberOfItems -> \(pasteboard.numberOfItems)")
-        //print("types -> \(pasteboard.types)")
-        //print("itemSet -> \(pasteboard.itemSet(withPasteboardTypes: pasteboard.types) ?? [])")
-        //print("string -> \(pasteboard.string ?? "")")
-        //print("url -> \(pasteboard.url ?? URL(string: ""))")
-        //print("image -> \(String(describing: pasteboard.image))")
+//        guard let itemSet = pasteboard.itemSet(withPasteboardTypes: pasteboard.types) else { return }
+//
+//        print("types -> \(pasteboard.types)")
+//        print("contains -> \(pasteboard.contains(pasteboardTypes: pasteboard.types, inItemSet: itemSet))")
+//
+//        for type in pasteboard.types {
+//            print("items -> \(pasteboard.data(forPasteboardType: type, inItemSet: itemSet))")
+//            print("items -> \(pasteboard.value(forPasteboardType: type))")
+//        }
         
+//        print("string -> \(pasteboard.string ?? "")")
+//        print("url -> \(pasteboard.url ?? URL(string: ""))")
+//        print("image -> \(String(describing: pasteboard.image))")
     }
+    
+    func fetchPreview(urlString: String){
+            guard let url = URL(string: urlString) else {
+                return
+            }
+            let linkPreview = LPLinkView()
+            let provider = LPMetadataProvider()
+            provider.startFetchingMetadata(for: url, completionHandler: {
+                [weak self] metaData, error in
+                guard let data = metaData, error == nil else{
+                    return
+                }
+                DispatchQueue.main.async {
+                    linkPreview.metadata = data
+                    print(linkPreview)
+                    self?.preview.addSubview(linkPreview)
+                    linkPreview.frame = CGRect(x: 0, y: 0, width: 360, height: 180)
+                    //self?.ivPreview.image = linkPreview.largeContentImage
+                }
+                
+            })
+        }
 }
 
 extension UIImageView {
